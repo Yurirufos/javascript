33ba61105991025e23c45e31450af8bef6c8cd0b
yurirufos <yurirufos@hotmail.com>
2023-01-07 19:12:31 -0300

    JS mais exercícios criei um desafio

---
+++ Desafio1/index.js
@@ -0,0 +1,5 @@
+
+
+let favoriteFood = 'Pizza'
+document.getElementById('text').innerHTML =  favoriteFood
+
--- novo JS/index.js
+++ novo JS/index.js
@@ -72,25 +72,49 @@ switch (airport) {
 
 // for loop consigo girar automaticamente contagens de numeros por exemplo :
 
-for( i = 1; i <= 20; i++) {
- console.log('for ' + i)
+for (i = 1; i <= 20; i++) {
+  console.log("for " + i);
 }
 
 // // Caso queira executar um loop em uma variavel que ja tenha sido feita no programa pode-se usar (While): por que sua variavel é externa ex:
 
-i = 1 
+i = 1;
 
-while ( i <= 10 ) {
- console.log('While ' + i)
- i++
+while (i <= 10) {
+  console.log("While " + i);
+  i++;
 }
 
 // variação de while loop usando "faça" ( do ) ex:
 
-i = 1
+i = 1;
 
 do {
- console.log('Do While ' + i)
- ++i
+  console.log("Do While " + i);
+  ++i;
+} while (i <= 10);
+
+// Mais uma variação de loop ( For in Loop)
+// essa variação consigo vizualizar informações e valores dentro de um objeto Ex Abaixo:
+
+const myCar = {
+  model: "BMW",
+  Color: "Red",
+};
+
+for (let i in myCar) 
+    console.log(i, myCar[i]);
+
+// Variação de loop ultitizada mais em Arrays ( For of Loop) exemplo abaixo de uma array e for of loop
+
+const friends = ['Flávia', 'Célia', 'Silvinha', 'Patrick', 'Bárbara', 'Gabi', 'Filipe'];
+
+for (let i of friends) 
+    console.log(i)
+
+// Break dentro de um loop exemplo (adicione um (if) se a variavel for === ao numero fecha parenteses pare (break))
+
+for( i = 1; i <= 10; i++) {
+  if ( i === 8 ) break
+  console.log('Num ' + i)
 }
-while( i <= 10)
